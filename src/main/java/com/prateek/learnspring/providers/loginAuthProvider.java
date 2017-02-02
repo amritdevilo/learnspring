a894a9efcfdbd74f502266471252e7a31d3847fd
Prateek Nischal <prateeknischal25@gmail.com>
2017-02-03 00:17:02 +0530

    in progress spring sec

---
+++ src/main/java/com/prateek/learnspring/providers/loginAuthProvider.java
@@ -0,0 +1,53 @@
+package com.prateek.learnspring.providers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+
+import com.prateek.learnspring.dao.UserDao;
+import com.prateek.learnspring.model.User;
+
+public class loginAuthProvider implements AuthenticationProvider{
+	
+	private UserDao userDao;
+	
+	public UserDao getUserDao() {
+		return userDao;
+	}
+	
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
+	
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		try {
+			String email = authentication.getName().trim();
+			String password = authentication.getCredentials().toString().trim();
+			
+			if (!userDao.isEmailExists(email)) {
+				return null;
+			}
+			User user = userDao.getUserByEmail(email);
+			
+			if (user.getPassword().equals(password))) {
+				List<GrantedAuthority> auths = new ArrayList<GrantedAuthority>();
+				auths.add(new SimpleGrantedAuthority())
+			}
+			
+		} catch (AuthenticationException e) {
+			throw e;
+		} catch (Exception e) {
+			e.printStackTrace()getClass();
+		}
+	}
+
+	public boolean supports(Class<?> authentication) {
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
+	}
+
+}
